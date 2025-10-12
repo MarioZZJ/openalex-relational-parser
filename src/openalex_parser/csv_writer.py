70226824c99bce7ec3a6f678f638eed187909e2a
tomleung1996 <tomleung1996@gmail.com>
2025-10-12 22:51:24 +0800

    all text field replace \t \r \n

--- src/openalex_parser/csv_writer.py
+++ src/openalex_parser/csv_writer.py
@@ -21,6 +21,9 @@ def _format_cell(value: Any) -> Any:
         return value.isoformat()
     if isinstance(value, Decimal):
         return format(value, "f")
+    if isinstance(value, str):
+        cleaned = " ".join(value.replace("\r", " ").replace("\n", " ").replace("\t", " ").split())
+        return cleaned
     return value
 
 
